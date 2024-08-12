b2df8d2e8b8a28355db20c721b06537f39c0e381
Jan Ong <35757911-jong15@users.noreply.replit.com>
2024-08-12 22:04:27 +0000

    Initial commit

--- index.js
+++ index.js
@@ -20,8 +20,23 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  
+  let date = new Date(req.params.date);
+  
+  if (date.toUTCString() === "Invalid Date"){
+    date = new Date(+req.params.date)}
+  if(date.toUTCString() === "Invalid Date") {res.json({ error : "Invalid Date" }); return}
+    
+    res.json({unix : date.getTime(), utc: date.toUTCString()})
+
+});
+
+// if empty string is passed, return current time
+app.get("/api", function (req, res) {
+
+    res.json({unix : new Date().getTime(), utc: new Date().toUTCString()})
+
 });
 
 
